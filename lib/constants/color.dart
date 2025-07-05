fb106bbb0c98a9856d05741e58b04a9702a000b9
Ar G Ho <41030992+iArgho@users.noreply.github.com>
2025-07-05 15:00:20 +0600

    5th July

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -5,6 +5,7 @@ class AppColors {
   static const primaryLight = Color(0xFFF2F7FD);
   static const lightGrey = Color(0xFFE6E6E6);
   static const surface = Color(0xFFFFFFFF);
+  static const surfaceGrey = Color(0xFFF4F4F5);
   static const secondary = Color(0xFFE6A516);
   static const textPrimary = Color(0xFF12253F);
   static const textLightGrey = Color(0xFF908F90);
--- lib/view/home/calculate/calculate_screen.dart
+++ lib/view/home/calculate/calculate_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:milk_mix/constants/color.dart';
+import 'package:milk_mix/view/widget/unit_selector_widget.dart';
 
-class CalculateScreen extends StatelessWidget {
+class CalculateScreen extends StatefulWidget {
   const CalculateScreen({super.key});
 
   @override
+  State<CalculateScreen> createState() => _CalculateScreenState();
+}
+
+class _CalculateScreenState extends State<CalculateScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.surface,
+      backgroundColor: AppColors.surfaceGrey,
       body: SafeArea(
         child: SingleChildScrollView(
           padding: EdgeInsets.symmetric(horizontal: 20.w),
@@ -19,13 +25,22 @@ class CalculateScreen extends StatelessWidget {
                 height: 100.h,
                 child: Container(
                   decoration: BoxDecoration(
-                    border: Border.all(color: AppColors.lightGrey, width: 1),
+                    border: Border.all(color: AppColors.lightGrey, width: 1.r),
                     borderRadius: BorderRadius.circular(10.r),
                   ),
                   alignment: Alignment.center,
-                  child: Text('Ads Only', style: TextStyle(fontSize: 16.sp)),
+                  child: Text(
+                    'Ads Only',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 10.sp,
+                      color: AppColors.textPrimary,
+                    ),
+                  ),
                 ),
               ),
+              SizedBox(height: 14.h),
+              const UnitSelectorWidget(),
             ],
           ),
         ),
