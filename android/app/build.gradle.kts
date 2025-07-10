3c84decd6b495862a6aeb4a582f48a590628bdb8
Ar G Ho <41030992+iArgho@users.noreply.github.com>
2025-07-10 10:40:01 +0600

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,14 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
     namespace = "com.example.milk_mix"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "34.0.0" // ← CORRECT
+    ndkVersion = ""34.0.0
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -13,7 +20,10 @@ android {
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.milk_mix"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -22,7 +32,13 @@ android {
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+flutter {
+    source = "../.."
 }
